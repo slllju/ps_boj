2e02dbafbbb70e3fdf4a1f3c03362e41a535258f
Seungju Yoo <65431881+slllju@users.noreply.github.com>
2020-06-20 16:28:14 +0900

    Create BOJ_2412_bone.cpp

---
+++ BOJ_2412_bone.cpp
@@ -0,0 +1,74 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+
+#define fir first
+#define sec second
+
+int main(){
+
+    // 1. 좌표 입력 받고 x좌표 기준 오름차순 완료.
+    int n, ey;
+    scanf("%d%d",&n,&ey);
+    vector<pair<int,int>> a;
+    a.push_back({0,0});
+    for( int i=0, x, y; i<n; i++ ){
+        scanf("%d%d",&x,&y);
+        a.push_back({x,y});
+    }
+    sort(a.begin(),a.end());
+
+
+    // 2. BFS를 위한 간선을 형성해본다
+    vector<int> ed[n+1];
+    for( int i=0; i<=n; i++ ){
+        for( int x=max(0,a[i].fir-2); x<=a[i].fir+2; x++ ){
+            int low=0, mid, high=n, sj=0, ej=n;
+            while( low<=high ){
+                mid = (low+high)/2;
+                if( a[mid].fir<x ){ sj = mid+1; low = mid+1;}
+                else high = mid-1;
+            }
+            if( a[sj].fir!=x ) continue;	// sj : 찾을 구간의 시작 인덱스
+            low = 0, high = n;
+            while( low<=high ){
+                mid = (low+high)/2;
+                if( a[mid].fir>x ){ ej = mid-1; high = mid-1; }
+                else low = mid+1;
+            }
+            if( ej<sj || a[ej].fir!=x ) continue;	// ej : 찾을 구간의 끝 인덱스
+            low = sj, high = ej;
+            while( low<=high ){
+                mid = (low+high)/2;
+                if( a[mid].sec<=a[i].sec ){ sj = mid; low = mid+1; }
+                else high = mid-1;
+            }
+            for( int j=max(0,sj-2), cnt=5; cnt--; j++ ){
+                if( a[j].fir==x && abs(a[j].sec-a[i].sec)<=2 && i!=j ){
+                    ed[i].push_back(j);
+                }
+            }
+        }
+    }
+
+    // 3. BFS를 하여 ey까지 올라가는 최소횟수를 구한다
+    int ans=-1;
+    queue<pair<int,int>> q;
+    q.push({0,0});
+    a[0].fir=-1;    // 방문 체크를 이렇게 하였네
+    while( !q.empty() ){
+        int u = q.front().fir, d = q.front().sec;
+        q.pop();
+        if( a[u].sec >= ey ){ ans = d; break; }
+        for( auto v : ed[u] ){
+            if( a[v].fir>=0 ){
+                a[v].fir = -1;
+                q.push({v,d+1});
+            }
+        }
+    }
+    printf("%d",ans);
+    return 0;
+}
